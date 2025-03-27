b03e3068e3f08695801deb0c6cdb004ccb2f90f6
ntnn19 <natannagar89@gmail.com>
2025-03-27 18:18:19 +0100

    preparing for pull request

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'biopython',
         'plotly',
         'streamlit-authenticator',  # Add streamlit-authenticator
-        'afusion',  # Assuming this is a separate package, though this could cause a circular dependency
         'git+https://github.com/ntnn19/AlphaFold3-GUI.git@original_main#egg=afusion',
     ],
     entry_points={
